a572e1f70b439224b1cba96792bae0440edab1b1
taskinoz <tristan@taskinoz.com>
2023-10-05 13:58:38 +1100

    Fixed incorrect states

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -16,39 +16,53 @@ const Settings = ({
     const [showSecret, setshowSecret] = useState(true);
 
     return (
-        <div
-            className="modal show"
-            style={{ display: 'block', position: 'absolute' }}
-        >
-            <Modal.Dialog>
-                <Modal.Header>
-                    <Modal.Title>API Keys</Modal.Title>
-                </Modal.Header>
-                <Form.Group as={Col} className="px-3 py-2">
-                    <Form.Label>Access Token:</Form.Label>
-                    <InputGroup>
-                        <Form.Control type={showSecret?"password":"text"} value={accessToken} onChange={e => setshowSecret(e.target.value)} />
-                        <Button
-                            aria-label="Show Token"
-                            onClick={() => setshowSecret(!showSecret)}
-                        ><FontAwesomeIcon icon={!showSecret?faEye:faEyeSlash} /></Button>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group as={Col} className="px-3 py-2">
-                    <Form.Label>App Secret:</Form.Label>
-                    <InputGroup>
-                        <Form.Control type={showToken?"password":"text"} value={appSecret} onChange={e => setShowToken(e.target.value)} />
-                        <Button
-                            aria-label="Show Secret" 
-                            onClick={() => setShowToken(!showToken)}
-                        ><FontAwesomeIcon icon={!showToken?faEye:faEyeSlash} /></Button>
-                    </InputGroup>
-                </Form.Group>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={() => setSettingsOpen(false)}>Save changes</Button>
-                </Modal.Footer>
-            </Modal.Dialog>
-        </div>
+      <div
+        className="modal show"
+        style={{ display: "block", position: "absolute" }}
+      >
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>API Keys</Modal.Title>
+          </Modal.Header>
+          <Form.Group as={Col} className="px-3 py-2">
+            <Form.Label>Access Token:</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type={showToken ? "password" : "text"}
+                value={accessToken}
+                onChange={(e) => setAccessToken(e.target.value)}
+              />
+              <Button
+                aria-label="Show Token"
+                onClick={() => setShowToken(!showToken)}
+              >
+                <FontAwesomeIcon icon={!showSecret ? faEye : faEyeSlash} />
+              </Button>
+            </InputGroup>
+          </Form.Group>
+          <Form.Group as={Col} className="px-3 py-2">
+            <Form.Label>App Secret:</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type={showSecret ? "password" : "text"}
+                value={appSecret}
+                onChange={(e) => setAppSecret(e.target.value)}
+              />
+              <Button
+                aria-label="Show Secret"
+                onClick={() => setshowSecret(!showSecret)}
+              >
+                <FontAwesomeIcon icon={!showToken ? faEye : faEyeSlash} />
+              </Button>
+            </InputGroup>
+          </Form.Group>
+          <Modal.Footer>
+            <Button variant="primary" onClick={() => setSettingsOpen(false)}>
+              Save changes
+            </Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </div>
     );
 }
 
